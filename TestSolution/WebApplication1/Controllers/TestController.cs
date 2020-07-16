d5824c1ce0a3f431765f100287ccd0eb2dfc0247
Aleksa <aleksa.kuzman.996@gmail.com>
2020-07-16 19:55:27 +0200

    ADD: Test endpoint

---
+++ TestSolution/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Noice");
+        }
+    }
+}
